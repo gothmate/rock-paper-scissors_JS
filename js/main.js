016429e7977ccb20e0d7a5fa1e8e82d4798c94e7
Gothmate <gothmate@gmail.com>
2021-10-22 21:56:15 -0300

    Adicionado resultado na tela.

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@ const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
 const possibleChoices = document.querySelectorAll('button')
 const chancesResult = document.querySelector('.botoes')
-
-const items = ['rock', 'paper', 'scissors']
+const items = ['paper', 'scissors', 'rock']
+let resultGame = document.getElementById('result-roll')
 let result = 0
 let userChoice
+let won = false
 
 possibleChoices.forEach(possibleChoice =>
   possibleChoice.addEventListener('click', e => {
@@ -27,36 +28,51 @@ function choose(clicado) {
   let compRandom = Math.round((Math.random() * 10) / 5)
   let player = clicado
 
-  optionSelected(compRandom, player)
-
   if (clicado == 'rock' && items[compRandom] == 'scissors') {
     result = 1
     score += result
+    won = true
   } else if (clicado == 'rock' && items[compRandom] == 'paper') {
     result = -1
     score += result
+    won = false
   } else if (clicado == 'paper' && items[compRandom] == 'rock') {
     result = 1
     score += result
+    won = true
   } else if (clicado == 'paper' && items[compRandom] == 'scissors') {
     result = -1
     score += result
+    won = false
   } else if (clicado == 'scissors' && items[compRandom] == 'paper') {
     result = 1
     score += result
+    won = true
   } else if (clicado == 'scissors' && items[compRandom] == 'rock') {
     result = -1
     score += result
+    won = false
   } else {
     result = 0
     score += result
+    won = undefined
   }
+  optionSelected(compRandom, player)
   document.querySelector('.player').innerHTML = `<h3>${score}</h3>`
 }
 
 function optionSelected(selectionComp, selectionPlayer) {
-  console.log(`Você selecionou ${selectionPlayer}`)
-  console.log(`O Jogo selecionou ${items[selectionComp]}`)
+  let winResult
+  if (won == true) {
+    winResult = 'You win!'
+  } else if (won == false) {
+    winResult = 'You lose.'
+  } else {
+    winResult = 'Draw game.'
+  }
+  resultGame.innerHTML = `<h3>You selected <span>${selectionPlayer}</span></h3>
+  <p>The game selected <span>${items[selectionComp]}</span></p>
+  <p>${winResult}</p>`
 }
 
 var newWindow
